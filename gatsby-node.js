4815ccff7b21209cc07d84dfc476fe0b9d4aef10
Morrison Cole <admin@morrisoncole.co.uk>
2021-02-18 20:04:56 +0900

    fix: sometimes goodreads returns no reviews, didn't really investigate just hacked a fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,194 +1,217 @@
-const crypto = require("crypto")
-const fetch = require("node-fetch")
-const queryString = require("query-string")
-const xml = require("xml2js")
+const crypto = require("crypto");
+const fetch = require("node-fetch");
+const queryString = require("query-string");
+const xml = require("xml2js");
 
-exports.sourceNodes = ({ actions, createNodeId}, configOptions) => {
-	const { createNode } = actions
-	
-	delete configOptions.plugins
+exports.sourceNodes = ({ actions, createNodeId }, configOptions) => {
+  const { createNode } = actions;
 
-	const processNode = (node) => {
-		const nodeId = node.id
-		const typeName = node.typeName
-		delete node.id
-		delete node.typeName
+  delete configOptions.plugins;
 
-		const nodeContent = JSON.stringify(node)
-		const nodeContentDigest = crypto
-			.createHash("md5")
-			.update(nodeContent)
-			.digest("hex")
+  const processNode = (node) => {
+    const nodeId = node.id;
+    const typeName = node.typeName;
+    delete node.id;
+    delete node.typeName;
 
-		const nodeData = Object.assign({}, node, {
-			id: nodeId,
-			parent: null,
-			children: [],
-			internal: {
-				type: typeName,
-				content: nodeContent,
-				contentDigest: nodeContentDigest
-			}
-		})
+    const nodeContent = JSON.stringify(node);
+    const nodeContentDigest = crypto
+      .createHash("md5")
+      .update(nodeContent)
+      .digest("hex");
 
-		if(nodeData.internal == null) {
-			console.log(nodeData)
-		}
+    const nodeData = Object.assign({}, node, {
+      id: nodeId,
+      parent: null,
+      children: [],
+      internal: {
+        type: typeName,
+        content: nodeContent,
+        contentDigest: nodeContentDigest,
+      },
+    });
 
-		return createNode(nodeData)
-	}
+    if (nodeData.internal == null) {
+      console.log(nodeData);
+    }
 
-	const apiFetch = async (url) => {
-		return fetch(url)
-			.then(response => {
-				return response.text()
-			})
-	}
+    return createNode(nodeData);
+  };
 
-	const unpack = (obj) => {
-		var unpacked = {}
-		for(var key in obj) {
-			unpacked[key] = obj[key][0]
+  const apiFetch = async (url) => {
+    return fetch(url).then((response) => {
+      return response.text();
+    });
+  };
 
-			const singularForms = {
-				shelves: "shelf",
-				authors: "author"
-			}
-			for( var plural in singularForms) {
-				if( (typeof unpacked[key] === "object") && key == plural) {
-					unpacked[key] = unpacked[key][singularForms[plural]].map(item => {
-						if( key != "shelves" ) {
-							return unpack(item)
-						} else {
-							return item["$"]
-						}
-					})
-				}
-			}
-			if( (typeof unpacked[key] === "object") && unpacked[key].hasOwnProperty("_") ) {
-        unpacked[key] = unpacked[key]["_"]
+  const unpack = (obj) => {
+    var unpacked = {};
+    for (var key in obj) {
+      unpacked[key] = obj[key][0];
+
+      const singularForms = {
+        shelves: "shelf",
+        authors: "author",
+      };
+      for (var plural in singularForms) {
+        if (typeof unpacked[key] === "object" && key == plural) {
+          unpacked[key] = unpacked[key][singularForms[plural]].map((item) => {
+            if (key != "shelves") {
+              return unpack(item);
+            } else {
+              return item["$"];
+            }
+          });
+        }
       }
-			if( (typeof unpacked[key] == "object") && unpacked[key].hasOwnProperty("$")) {
-				unpacked[key] = unpacked[key]["$"]
-			}
-			if( (typeof unpacked[key] === "object") && !Array.isArray(unpacked[key]) && !unpacked[key].hasOwnProperty("_")) {
-				unpacked[key] = unpack(unpacked[key])
-			}
-			if( key.startsWith("isbn") && (typeof unpacked[key] == "object") && (unpacked[key].nil == "t")) {
-				unpacked[key] = ''
-			}
-		}
-		return unpacked
-	}
+      if (
+        typeof unpacked[key] === "object" &&
+        unpacked[key].hasOwnProperty("_")
+      ) {
+        unpacked[key] = unpacked[key]["_"];
+      }
+      if (
+        typeof unpacked[key] == "object" &&
+        unpacked[key].hasOwnProperty("$")
+      ) {
+        unpacked[key] = unpacked[key]["$"];
+      }
+      if (
+        typeof unpacked[key] === "object" &&
+        !Array.isArray(unpacked[key]) &&
+        !unpacked[key].hasOwnProperty("_")
+      ) {
+        unpacked[key] = unpack(unpacked[key]);
+      }
+      if (
+        key.startsWith("isbn") &&
+        typeof unpacked[key] == "object" &&
+        unpacked[key].nil == "t"
+      ) {
+        unpacked[key] = "";
+      }
+    }
+    return unpacked;
+  };
 
-	return new Promise( async (resolve, reject) => {
-		var reviews = {}
-		var authors = {}
-		var books = {}
-		var shelves = {}
+  return new Promise(async (resolve, reject) => {
+    var reviews = {};
+    var authors = {};
+    var books = {};
+    var shelves = {};
 
-		var sourceDone = false
-		var pageNum = 1
-		var raw
+    var sourceDone = false;
+    var pageNum = 1;
+    var raw;
 
-		while(!sourceDone) {
-			const roundOptions = {
-				page: pageNum,
-				per_page: 200,
-				v: 2,
-			}
+    while (!sourceDone) {
+      const roundOptions = {
+        page: pageNum,
+        per_page: 200,
+        v: 2,
+      };
 
-			const pageOptions = Object.assign({}, configOptions, roundOptions)
-			const apiOptions = queryString.stringify(pageOptions)
-			const apiUrl = `https://www.goodreads.com/review/list?${apiOptions}`
-			//console.log(`\nFetch Goodreads:\n\t${apiUrl}\n`)
+      const pageOptions = Object.assign({}, configOptions, roundOptions);
+      const apiOptions = queryString.stringify(pageOptions);
+      const apiUrl = `https://www.goodreads.com/review/list?${apiOptions}`;
+      console.log(`\nFetch Goodreads:\n\t${apiUrl}\n`);
 
-			raw = await apiFetch(apiUrl)
+      raw = await apiFetch(apiUrl);
 
-			xml.parseString(raw, { trim: true }, (error, result) => {
-				if(error) {
-					return reject("Invalid response from Goodreads: XML failed to parse")
-				}
+      xml.parseString(raw, { trim: true }, (error, result) => {
+        if (error) {
+          return reject("Invalid response from Goodreads: XML failed to parse");
+        }
 
-				if( !(result.hasOwnProperty("GoodreadsResponse") 
-					&& result.GoodreadsResponse.hasOwnProperty("reviews")
-					&& Array.isArray(result.GoodreadsResponse.reviews)
-					&& (result.GoodreadsResponse.reviews.length == 1))) {
-					return reject("Invalid response from Goodreads: unexpected XML structure")
-				}
+        if (
+          !(
+            result.hasOwnProperty("GoodreadsResponse") &&
+            result.GoodreadsResponse.hasOwnProperty("reviews") &&
+            Array.isArray(result.GoodreadsResponse.reviews) &&
+            result.GoodreadsResponse.reviews.length == 1
+          )
+        ) {
+          return reject(
+            "Invalid response from Goodreads: unexpected XML structure"
+          );
+        }
 
-				const response = result.GoodreadsResponse.reviews[0]
+        const response = result.GoodreadsResponse.reviews[0];
 
-				response.review.forEach(rawReview => {
-					var review = unpack(rawReview)
-	
-					var book = review.book
-					var bookAuthors = review.book.authors
-					var reviewShelves = review.shelves
+        if (!response.reviews) {
+          sourceDone = true;
+        } else {
+          response.review.forEach((rawReview) => {
+            var review = unpack(rawReview);
 
-					delete review.book
-					delete review.shelves
-					delete book.authors
+            var book = review.book;
+            var bookAuthors = review.book.authors;
+            var reviewShelves = review.shelves;
 
-					const reviewId = createNodeId(`goodreads-review-${review.id}`)
-					review.goodreadsId = review.id
-					review.id = reviewId
-					review.typeName = "GoodreadsReview"
+            delete review.book;
+            delete review.shelves;
+            delete book.authors;
 
-					const bookId = createNodeId(`goodreads-book-${book.id}`)
-					if(!books[bookId]) {
-						book.goodreadsId = book.id
-						book.id = bookId
-						book.typeName = "GoodreadsBook"
-						book.reviews___NODE = []
-						book.authors___NODE = []
-						books[bookId] = book
-					} 
-					books[bookId].reviews___NODE.push(reviewId)
-					review.book___NODE = bookId
+            const reviewId = createNodeId(`goodreads-review-${review.id}`);
+            review.goodreadsId = review.id;
+            review.id = reviewId;
+            review.typeName = "GoodreadsReview";
 
-					bookAuthors.forEach(author => {
-						const authorId = createNodeId(`goodreads-author-${author.id}`)
-						if(!authors[authorId]) {
-							author.goodreadsId = author.id
-							author.id = authorId
-							author.typeName = "GoodreadsAuthor"
-							author.books___NODE = []
-							authors[authorId] = author
-						}
-						authors[authorId].books___NODE.push(bookId)
-						books[bookId].authors___NODE.push(authorId)
-					})
+            const bookId = createNodeId(`goodreads-book-${book.id}`);
+            if (!books[bookId]) {
+              book.goodreadsId = book.id;
+              book.id = bookId;
+              book.typeName = "GoodreadsBook";
+              book.reviews___NODE = [];
+              book.authors___NODE = [];
+              books[bookId] = book;
+            }
+            books[bookId].reviews___NODE.push(reviewId);
+            review.book___NODE = bookId;
 
-					reviewShelves.forEach(shelf => {
-						const shelfId = createNodeId(`goodreads-shelf-${shelf.id}`)
-						if(!shelves[shelfId]) {
-							shelf.goodreadsId = shelf.id
-							shelf.id = shelfId
-							shelf.typeName = "GoodreadsShelf"
-							shelf.reviews___NODE = []
-							shelves[shelfId] = shelf
-						} 
-						shelves[shelfId].reviews___NODE.push(reviewId)
-					})
-					
-					reviews[reviewId] = review
-				})
-				
-				if(response["$"]["end"] == response["$"]["total"]) {
-					sourceDone = true
-				} else {
-					pageNum += 1
-				}
-			})
-		}
-		
-		[reviews, authors, books, shelves].forEach(collection => {
-			for(var key in collection) {
-				processNode(collection[key])
-			}
-		})
+            bookAuthors.forEach((author) => {
+              const authorId = createNodeId(`goodreads-author-${author.id}`);
+              if (!authors[authorId]) {
+                author.goodreadsId = author.id;
+                author.id = authorId;
+                author.typeName = "GoodreadsAuthor";
+                author.books___NODE = [];
+                authors[authorId] = author;
+              }
+              authors[authorId].books___NODE.push(bookId);
+              books[bookId].authors___NODE.push(authorId);
+            });
 
-		resolve()
-	})
-}
+            reviewShelves.forEach((shelf) => {
+              const shelfId = createNodeId(`goodreads-shelf-${shelf.id}`);
+              if (!shelves[shelfId]) {
+                shelf.goodreadsId = shelf.id;
+                shelf.id = shelfId;
+                shelf.typeName = "GoodreadsShelf";
+                shelf.reviews___NODE = [];
+                shelves[shelfId] = shelf;
+              }
+              shelves[shelfId].reviews___NODE.push(reviewId);
+            });
+
+            reviews[reviewId] = review;
+          });
+
+          if (response["$"]["end"] == response["$"]["total"]) {
+            sourceDone = true;
+          } else {
+            pageNum += 1;
+          }
+        }
+      });
+    }
+
+    [reviews, authors, books, shelves].forEach((collection) => {
+      for (var key in collection) {
+        processNode(collection[key]);
+      }
+    });
+
+    resolve();
+  });
+};
